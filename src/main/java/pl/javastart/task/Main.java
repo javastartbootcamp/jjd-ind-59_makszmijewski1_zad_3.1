3f64076e2cfddbcd898f6a4bc6858b60d98e0c70
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-02 18:16:29 +0100

    drugi commit

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,16 +3,18 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        ChristmasTree christmasTree1 = new ChristmasTree();
-        ChristmasTree christmasTree2 = new ChristmasTree();
+        ChristmasTree christmasTree1 = new ChristmasTree(185,"sosna",25);
+        ChristmasTree christmasTree2 = new ChristmasTree(160,"jodła",23);
 
-        christmasTree1.setHeight(185);
+       /* christmasTree1.setHeight(185);
         christmasTree1.setType("sosna");
         christmasTree1.setBaublesAmount(25);
 
         christmasTree2.setHeight(160);
         christmasTree2.setType("jodła");
         christmasTree2.setBaublesAmount(23);
+        
+        */
 
         System.out.println(christmasTree1);
         System.out.println(christmasTree2);
